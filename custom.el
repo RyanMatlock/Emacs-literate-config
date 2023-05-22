b00ee26f9bd7e1d6b8b9063769c510d8eaf6d8f6
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-21 20:37:02 -0700

    :coffin: remove commented out lines
    This was done automatically when ansi-color-names-vector was automatically
    updated.

--- custom.el
+++ custom.el
@@ -16,7 +16,6 @@
  '(custom-safe-themes
    '("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))
  '(ebib-bibtex-dialect 'biblatex)
- ;; '(emojify-display-style 'image)
  '(eshell-cmpl-cycle-completions nil)
  '(fci-rule-color "#073642")
  '(flycheck-python-flake8-executable "python3")
@@ -46,23 +45,12 @@
    '("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00"))
  '(hl-fg-colors
    '("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36"))
- ;; '(ido-enable-flex-matching t)
- ;; '(ido-everywhere t)
- ;; '(ido-file-extensions-order '(".org" ".py" ".tex" ".el" ".txt" ".yaml" ".js" ".md"))
- ;; '(initial-major-mode 'text-mode)
- ;; '(initial-scratch-message nil)
  '(magit-diff-use-overlays nil)
  '(org-adapt-indentation nil)
  '(org-export-backends '(ascii beamer html icalendar latex md odt))
  '(org-export-with-smart-quotes t)
- ;; '(org-hide-emphasis-markers t)
  '(org-hide-leading-stars nil)
  '(org-highlight-latex-and-related '(native))
- ;; '(org-journal-date-format "%A, %F")
- ;; '(org-journal-dir "~/Dropbox/journal/")
- ;; '(org-journal-file-format "%Y/%m/week-%V-(%Y-%m-%d).org")
- ;; '(org-journal-file-type 'weekly)
- ;; '(org-journal-time-format "[%I:%M%P]")
  '(org-latex-default-packages-alist
    '(("AUTO" "inputenc" t
       ("pdflatex"))
@@ -80,36 +68,10 @@
      ("" "capt-of" nil nil)
      ("colorlinks=true" "hyperref" nil nil)
      ("" "siunitx" nil nil)))
- ;; '(org-mode-hook
- ;;   '(flyspell-mode
- ;;     #[0 "\300\301\302\303\304$\207"
- ;;         [add-hook change-major-mode-hook org-show-all append local]
- ;;         5]
- ;;     #[0 "\300\301\302\303\304$\207"
- ;;         [add-hook change-major-mode-hook org-babel-show-result-all append local]
- ;;         5]
- ;;     org-babel-result-hide-spec org-babel-hide-all-hashes
- ;;     #[0 "\301\211\207"
- ;;         [imenu-create-index-function org-imenu-get-tree]
- ;;         2]
- ;;     (lambda nil
- ;;       (hs-minor-mode))
- ;;     org-journal-update-auto-mode-alist))
- ;; '(org-pretty-entities t)
- ;; '(package-archives
- ;;   '(("melpa" . "https://melpa.org/packages/")
- ;;     ("gnu" . "https://elpa.gnu.org/packages/")))
- ;; '(package-selected-packages
- ;;   '(spice-mode python-mode use-package hindent lsp-jedi lsp-latex lsp-pyright lsp-haskell flymake-hlint haskell-tab-indent mediawiki vterm osx-clipboard company-org-roam org-roam org-roam-bibtex org-roam-server org-scrum chess company-lsp osx-dictionary lsp-ui flycheck-rust lsp-mode rust-mode toml-mode elpy pomidor unfill sql-indent sqlup-mode db-pg latex-math-preview org-ref ox-reveal highlight-indent-guides org-journal org-journal-list ox-pandoc ox-rst ox-tufte ox-wk org-re-reveal lorem-ipsum company-anaconda company-bibtex company-emoji company-math company-shell company-web telephone-line magit magit-filenotify transient solarized-theme theme-looper flx flx-ido ido-yes-or-no ivy-yasnippet emoji-display ox-epub ivy ebib hideshow-org s ob-ipython ein electric-case electric-operator electric-spacing elein eldoc-overlay eldoc-eval ac-slime elisp-slime-nav slime auto-complete emojify yaml-mode wgrep-ack wget web-beautify tagedit sx scion pytest paredit pandoc-mode org-bullets org-ac nodejs-repl json-mode js3-mode iedit help-mode+ help-fns+ help+ helm-bibtex gist flycheck-pyflakes exec-path-from-shell d-mode company-auctex cider blank-mode bison-mode better-defaults awk-it auto-complete-chunk auto-complete-c-headers auctex-latexmk arduino-mode ac-python ac-js2 ac-geiser))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
  '(show-trailing-whitespace t)
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
- ;; '(solarized-distinct-fringe-background t)
- ;; '(solarized-high-contrast-mode-line t)
- ;; '(solarized-scale-org-headlines nil)
- ;; '(solarized-use-more-italic t)
- ;; '(solarized-use-variable-pitch nil)
  '(term-default-bg-color "#002b36")
  '(term-default-fg-color "#839496")
  '(text-mode-hook
